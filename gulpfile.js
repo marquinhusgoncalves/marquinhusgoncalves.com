51dfb40b0153d227d1aa3d7a4243a78dbea9ceb4
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-07-06 01:17:54 -0300

    new header

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp        = require('gulp');
+const gutil       = require('gulp-util');
+const sass        = require('gulp-sass');
+const prefix      = require('gulp-autoprefixer');
+const cssnano     = require('gulp-cssnano');
+const concat      = require('gulp-concat');
+const uglify      = require('gulp-uglify');
+const browserSync = require('browser-sync').create();''
+const child       = require('child_process');
+
+const siteRoot = '_site';
+const cssFiles = '_css/**/*.?(s)css';
+
+gulp.task('css', () => {
+  gulp.src(cssFiles)
+    .pipe(sass())
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('default', ['css', 'jekyll', 'serve']);
