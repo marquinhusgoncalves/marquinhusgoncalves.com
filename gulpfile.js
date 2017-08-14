484549b58cd31adfd7f8c4e9c4bc4cf4465a077b
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-08-14 20:59:57 -0300

    Add news config in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp        = require('gulp');
+const plumber     = require('gulp-plumber');
 const gutil       = require('gulp-util');
 const sass        = require('gulp-sass');
 const prefix      = require('gulp-autoprefixer');
@@ -7,15 +8,45 @@ const concat      = require('gulp-concat');
 const uglify      = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 const child       = require('child_process');
+const sourcemaps  = require('gulp-sourcemaps');
+const imagemin    = require('gulp-imagemin');
 
 const siteRoot = '_site';
-const cssFiles = '_css/**/*.?(s)css';
+const cssFiles = 'assets/css/**/*.?(s)css';
 
 gulp.task('css', () => {
-  gulp.src(cssFiles)
-    .pipe(sass())
-    .pipe(concat('all.css'))
-    .pipe(gulp.dest('assets'));
+  gulp.src('src/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('js', () => {
+  return gulp.src('src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/'))
+});
+
+gulp.task('imagemin', () => {
+  return gulp.src('src/img/**/*')
+    .pipe(plumber())
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('assets/img/'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src([
+    'src/fonts/**'])
+    .pipe(gulp.dest('assets/fonts/'));
 });
 
 gulp.task('jekyll', () => {
@@ -47,4 +78,4 @@ gulp.task('serve', () => {
   gulp.watch(cssFiles, ['css']);
 });
 
-gulp.task('default', ['css', 'jekyll', 'serve']);
+gulp.task('default', ['css', 'js', 'imagemin', 'fonts', 'jekyll', 'serve']);
