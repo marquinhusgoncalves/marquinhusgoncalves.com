464c2cd57f9b8eb918a53dce02ff2af9d84233bf
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-08-15 14:50:51 -0300

    Adjust gulpfile and rename colors

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,17 @@ const prefix      = require('gulp-autoprefixer');
 const cssnano     = require('gulp-cssnano');
 const concat      = require('gulp-concat');
 const uglify      = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
-const child       = require('child_process');
+const browserSync = require('browser-sync');
+const cp          = require('child_process');
 const sourcemaps  = require('gulp-sourcemaps');
 const imagemin    = require('gulp-imagemin');
 
-const siteRoot = '_site';
-const cssFiles = 'assets/css/**/*.?(s)css';
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
 gulp.task('css', () => {
-  gulp.src('src/scss/style.scss')
+  gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({
@@ -49,33 +50,40 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('assets/fonts/'));
 });
 
-gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['build',
-    '--watch',
-    '--incremental',
-    '--drafts'
-  ]);
-
-  const jekyllLogger = (buffer) => {
-    buffer.toString()
-      .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
+/**
+ * Monta o site do Jekyll
+ */
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+      .on('close', done);
 });
 
-gulp.task('serve', () => {
-  browserSync.init({
-    files: [siteRoot + '/**'],
-    port: 4000,
-    server: {
-      baseDir: siteRoot
-    }
+/**
+* Refaz o site e atualiza a página
+*/
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+/**
+* Espera até que o jekyll-build seja executado e então levanta o
+* servidor utilizando o _site como pasta raiz
+*/
+gulp.task('browser-sync', ['jekyll-build'], function() {
+  browserSync({
+      server: {
+          baseDir: '_site'
+      }
   });
-
-  gulp.watch(cssFiles, ['css']);
 });
 
-gulp.task('default', ['css', 'js', 'imagemin', 'fonts', 'jekyll', 'serve']);
+gulp.task('watch', function () {
+  gulp.watch('src/scss/**/*.scss', ['css', 'jekyll-rebuild']);
+  gulp.watch('src/js/**/*.js', ['js', 'jekyll-rebuild']);
+  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin', 'jekyll-rebuild']);
+  gulp.watch('src/fonts/**/*.{eot,svg,ttf, woff}', ['fonts', 'jekyll-rebuild']);
+  gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['css', 'js', 'imagemin', 'fonts', 'browser-sync', 'watch']);
