c51ee62778403c1dde2b1cf2f6a9380aac600740
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-06-01 20:17:10 -0300

    refactor: Refactor in JS file

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -1,25 +1,27 @@
-window.addEventListener('scroll', function () {
+function showButton() {
   if (this.scrollY >= 50) {
     document.querySelector('#return-to-top').classList.add('visible');
   } else {
     document.querySelector('#return-to-top').classList.remove('visible');
   }
-});
+}
+
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  const difference = to - element.scrollTop;
+  const perTick = difference / duration * 10;
+
+  setTimeout(function () {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
 
 function runScroll() {
   scrollTo(document.body, 0, 600);
 }
-var scrollme = document.querySelector('#return-to-top');
-scrollme.addEventListener('click', runScroll, false)
+const scrollme = document.querySelector('#return-to-top');
+scrollme.addEventListener('click', runScroll);
 
-function scrollTo(element, to, duration) {
-  if (duration <= 0) return;
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
-
-  setTimeout(function () {
-    element.scrollTop = element.scrollTop + perTick;
-    if (element.scrollTop == to) return;
-    scrollTo(element, to, duration - 10);
-  }, 10);
-}
+window.addEventListener('scroll', showButton);
