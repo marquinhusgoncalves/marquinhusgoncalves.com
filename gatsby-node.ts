339dc7d9e57dd2dc6df1c6a0bdecdf40ba9503c3
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2023-12-29 19:58:57 +0000

    refactor: to adjust projects and posts

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,26 +1,26 @@
-const path = require('path');
+const path = require("path");
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }: any) => {
   const { createNodeField } = actions;
   // Ensures we are processing only markdown files
-  if (node.internal.type === 'MarkdownRemark') {
+  if (node.internal.type === "MarkdownRemark") {
     const collection = getNode(node.parent).sourceInstanceName;
     const slug = createFilePath({
       node,
       getNode,
-      basePath: `pages`,
+      basePath: "pages",
     });
     createNodeField({
       node,
-      name: 'collection',
+      name: "collection",
       value: collection,
     });
     createNodeField({
       node,
-      name: `slug`,
-      value: `/${slug.slice(12)}`,
+      name: "slug",
+      value: slug,
     });
   }
 };
@@ -64,7 +64,7 @@ exports.createPages = ({ graphql, actions }: any) => {
     posts.forEach(({ node, next, previous }: any) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve(`./src/templates/post.tsx`),
+        component: path.resolve("./src/templates/post.tsx"),
         context: {
           // Data passed to context is available
           // in page queries as GraphQL variables.
--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -1,55 +1,59 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 // import SEO from '../../../src/components/Seo';
-import Titles from '../components/Titles';
-import Card from '../components/Card';
+import Titles from "../components/Titles";
+import Card from "../components/Card";
 
 const Projetos = ({ data }: any) => {
-  // const projectsList = data.allMarkdownRemark.edges;
+  const projectsList = data.allMarkdownRemark.edges;
   return (
     <Layout>
       {/* <SEO title="Projetos" /> */}
       <Titles title="Projetos" />
-      {/* {projectsList.map(
+      {projectsList.map(
         ({
           node: {
             frontmatter: { title, description, href },
           },
-        }: {node: { frontmatter: { title: string, description: string, href: string } } } ) => (
+        }: {
+          node: {
+            frontmatter: { title: string; description: string; href: string };
+          };
+        }) => (
           <Card
             key={title}
             title={title}
             description={description}
             href={href}
           />
-        ),
-      )} */}
+        )
+      )}
     </Layout>
   );
 };
 
-// export const query = graphql`
-//   {
-//     allMarkdownRemark(
-//       filter: { fields: { collection: { eq: "projects" } } }
-//       sort: { fields: [frontmatter___date], order: DESC }
-//     ) {
-//       edges {
-//         node {
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             title
-//             description
-//             href
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  {
+    allMarkdownRemark(
+      filter: { fields: { collection: { eq: "projects" } } }
+      sort: { frontmatter: { date: DESC } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            description
+            href
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Projetos;
