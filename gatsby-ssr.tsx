223fd70dd97b4f6f6bbce44996c312757bc70735
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-02-03 18:12:30 +0000

    feat: add adsense

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,17 @@
+import { GatsbySSR } from 'gatsby';
+import React from 'react';
+
+export const onRenderBody: GatsbySSR['onRenderBody'] = ({
+  setHeadComponents,
+}) => {
+  const HeadComponents = [
+    <script
+      key="adsense"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8901672052848512"
+      crossOrigin="anonymous"
+    />,
+  ];
+
+  return setHeadComponents(HeadComponents);
+};
